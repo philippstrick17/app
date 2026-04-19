71ddd71b1b839e86719fdab0faeeab198663e438
philippstrick17 <philipp@salgert.de>
2026-04-19 21:09:04 +0200

    Halloooo

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.myapplication
+
+
+import android.media.SoundPool
+import android.os.Build
+import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.selects.SelectInstance
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var soundPool: SoundPool
+    private var soundId: Int = 0
+    private var counter = 0
+
+    private fun vibrate() {
+        val vibrator = getSystemService(VIBRATOR_SERVICE) as Vibrator
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            vibrator.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            vibrator.vibrate(50)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val tvCounter = findViewById<TextView>(R.id.counterText)
+        val btnClick = findViewById<Button>(R.id.meinErsterButton)
+        val resetClick = findViewById<Button>(R.id.resetbutton)
+
+        resetClick.setOnClickListener {
+            counter = 0
+            tvCounter.text = counter.toString()
+            tvCounter.setTextColor(android.graphics.Color.BLACK)
+        }
+
+        btnClick.setOnClickListener {
+            counter ++
+            tvCounter.text = counter.toString()
+            vibrate()
+
+            if (counter >= 10) {
+                tvCounter.setTextColor(android.graphics.Color.RED)
+            }
+        }
+    }
+}
